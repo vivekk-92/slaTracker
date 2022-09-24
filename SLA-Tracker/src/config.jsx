3e0d822c58c3edb2fe6630e7102057f7bdbc5b61
Vivek Kumar <vivek.kumar1@olx.com>
2022-09-24 12:23:52 +0530

    Changes to add Escalation Matrix section

--- SLA-Tracker/src/config.jsx
+++ SLA-Tracker/src/config.jsx
@@ -7,8 +7,16 @@ import ForgeUI, {
     Option,
     useState,
     SectionMessage,
+    Table,
+    Head,
+    Cell,
+    Row,
+    TextField,
+    Text,
+    UserPicker,
+    TextArea
 } from '@forge/ui';
-import {TIME_OPTIONS,STORAGE_KEY_PREFIX} from './constants';
+import {TIME_OPTIONS, STORAGE_KEY_PREFIX, Week_DAYS, ISSUE_SEVERITY} from './constants';
 
 
 import { storage } from '@forge/api';
@@ -23,20 +31,96 @@ const App = () => {
         setConfigSubmitted(true);
     };
 
-    const renderDateTimeConfig = (shiftLabel, shiftName) => (
+    const renderWorkingHours = (shiftLabel, shiftName) => (
         <Select label= {shiftLabel} name={shiftName}>
             {Object.values(TIME_OPTIONS).map(option => <Option label={option + " AM"} value={option + " AM"} />)}
             {Object.values(TIME_OPTIONS).map(option => <Option label={option + " PM"} value={option + " PM"} />)}
         </Select>
     )
 
+    const renderWorkingDays = () => (
+        <Select label= "Choose Working Days of a week" name="WorkingDays" isMulti={true}>
+            {Object.values(Week_DAYS).map(option => <Option label={option} value={option} />)}
+        </Select>
+    )
+
+    const renderSLAMapping = () => (
+        <Table>
+            <Head>
+                <Cell>
+                    <Text>Please provide SLA mapping for the issues</Text>
+                </Cell>
+            </Head>
+            <Row>
+                <Cell>
+                    <Text>Issue Severity</Text>
+                </Cell>
+                <Cell>
+                    <Text>SLA</Text>
+                </Cell>
+            </Row>
+            {Object.values(ISSUE_SEVERITY).map(issue => (
+                <Row>
+                    <Cell>
+                        <Text>{issue}</Text>
+                    </Cell>
+                    <Cell>
+                        <TextField label="" name={issue} />
+                    </Cell>
+                </Row>
+            ))}
+        </Table>
+    )
+
+    const renderWorkingHoursAndDays = () => (
+        <Table>
+            <Head>
+                <Cell>
+                    <Text>Please Choose Working Hours and Days for your team</Text>
+                </Cell>
+            </Head>
+                <Row>
+                    <Cell>
+                        {renderWorkingHours("Choose Working Hours Start Time","startTime")}
+                    </Cell>
+                    <Cell>
+                        {renderWorkingHours("Choose Working Hours End Time","endTime")}
+                    </Cell>
+                    <Cell>
+                        {renderWorkingDays()}
+                    </Cell>
+                </Row>
+
+        </Table>
+    )
+
+    const renderEscalationMatrix = () => (
+        <Table>
+            <Head>
+                <Cell>
+                    <Text>Please select Escalation Levels</Text>
+                </Cell>
+            </Head>
+                <Row>
+                    <Cell>
+                        <UserPicker label="Escalation Level 1" name="escalationLevel1" />
+                    </Cell>
+                </Row>
+            <Row>
+                <Cell>
+                    <UserPicker label="Escalation Level 2" name="escalationLevel2" />
+                </Cell>
+            </Row>
+        </Table>
+    )
 
     return (
         <Fragment>
             {isConfigSubmitted && <SectionMessage title="Configuration Saved" appearance="confirmation"/>}
             {<Form onSubmit={onConfigSubmit}>
-                {renderDateTimeConfig("Choose Working Hours Start Time","startTime")}
-                {renderDateTimeConfig("Choose Working Hours End Time","endTime")}
+                {renderWorkingHoursAndDays()}
+                {renderSLAMapping()}
+                {renderEscalationMatrix()}
             </Form>}
         </Fragment>
     );
--- SLA-Tracker/src/constants.js
+++ SLA-Tracker/src/constants.js
@@ -3,4 +3,8 @@ export const STORAGE_KEY_PREFIX = 'SLA_TRACKER_CONFIG';
 
 export const TIME_OPTIONS = ["1:00","2:00","3:00","4:00","5:00","6:00","7:00","8:00","9:00","10:00","11:00","12:00"];
 
+export const Week_DAYS = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+
+export const ISSUE_SEVERITY = ['Critical','High','Medium','Low'];
+
 
